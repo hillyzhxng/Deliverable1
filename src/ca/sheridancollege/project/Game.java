9f0bff3e587328b721f3fff998a4c24c75ac4710
Hillary Zhang <zhanghi@sheridancollege.ca>
2023-03-03 14:07:57 -0500

    added fixed comment

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -2,6 +2,10 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
+ **hillary zhang
+ * rachel prasad
+ * rishabh kumar
+ * march 3, 2023
  */
 package ca.sheridancollege.project;
 
